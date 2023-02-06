1a32ddb4ba7b8565db792f474d3d41cefc07a1ac
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:15:29 -0400

    Create Exercício 57.py

---
+++ Exercício 57.py
@@ -0,0 +1,15 @@
+numero_da_placa = int(input("Digite o número da placa:\n"))
+if numero_da_placa == 0 or numero_da_placa == 1:
+    print("Não circular segunda-feira\n")
+
+elif numero_da_placa == 2 or numero_da_placa ==3:
+    print("Não circular terça-feira\n")
+
+elif numero_da_placa == 4 or numero_da_placa ==5:
+    print("Não circular quarta-feira")
+
+elif numero_da_placa== 6 or numero_da_placa ==7:
+    print("Não circular quinta-feira\n")
+
+elif numero_da_placa == 8 or numero_da_placa ==9:
+    print("Não circular sexta-feira\n")
