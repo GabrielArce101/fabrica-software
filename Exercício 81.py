714ae5c61425892bc30d5fdd13295de9f07d61f4
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:25:08 -0400

    Create Exercício 81.py

---
+++ Exercício 81.py
@@ -0,0 +1,20 @@
+dados_bebe = {'Nome da mãe':input("Digite o nome da mãe desse bebe:\n"),
+    "Nome do bebe:": input("Digite o nome do bebe:\n"),
+    'Data de nascimento':(input("Digite a data de nascimento:\n")),
+    'Peso após o nascimento': input("Digite o peso após o nascimento:\n"),
+    "Altura:":(input("Qual o tamanho do bebe:\n"))}
+print(dados_bebe)
+print('='*300)
+dados_mae={
+    "Nome":input("Digite o nome da mãe:\n"),
+    "Endereço":input("Digite o endereço:\n"),
+    "Telefone":input("Digite o telefone para contato:\n"),
+    "Data de nascimento da mãe": input("Digite a data de nascimento da mãe:\n")}
+print(dados_mae)
+print('='*300)
+dados_medico = {
+    'Nome': input("Digite o nome:\n"),
+    'CRM': int(input("Digite o CRM:\n")),
+    'Telefone': int(input("Digite o telefone do médico:\n")),
+    'Especialidade': input("Qual a especialidade do médico:\n")}
+print(dados_medico)
