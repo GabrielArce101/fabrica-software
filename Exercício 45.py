25053c89e907f55dacd5c7d2da6ec64ac3e49620
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:10:09 -0400

    Create Exercício 45.py

---
+++ Exercício 45.py
@@ -0,0 +1,11 @@
+nasceu = int(input("Digite o ano em que essa pessoa nasceu:\n"))
+
+dias = nasceu*365
+semanas = nasceu*4
+meses = nasceu*12
+anos = 2022-nasceu
+
+print(dias)
+print(semanas)
+print(meses)
+print(anos)
