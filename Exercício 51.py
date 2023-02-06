979d1b6b5f11f4af45987cf9f79aafe74575c1c3
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:12:35 -0400

    Create Exercício 51.py

---
+++ Exercício 51.py
@@ -0,0 +1,7 @@
+numero =int(input("Digite um número:\n"))
+soma =0
+while numero !=-999:
+    soma= soma+numero
+    numero =int(input("Digite um número:\n"))
+    if numero==-999:
+        print(soma)
