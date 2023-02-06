f895a7c51e9d5b141d136b00ace22ce205c91c6b
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:23:14 -0400

    Create Exercício 76.py

---
+++ Exercício 76.py
@@ -0,0 +1,9 @@
+#Faça um Programa que leia 4 notas, mostre as notas e a média na tela.
+nota1=4.5
+nota2=5.0
+nota3=8.0
+nota4=6.0
+soma=nota1+nota2+nota3+nota4
+media=(soma/4)
+print(soma)
+print(f'A média dessas notas é de:\n{media}')
