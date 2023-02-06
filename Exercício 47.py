5fb846a53a12cd27756d413ec7c9c465796613b8
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:11:05 -0400

    Create Exercício 47.py

---
+++ Exercício 47.py
@@ -0,0 +1,10 @@
+num1 = int(input("Digite um número:\n"))
+num2 = int(input("Digite um número:\n"))
+
+if num1 == num2:
+    soma = num1+num2
+    print(soma)
+
+else:
+    mult = num1*num2
+    print(mult)
