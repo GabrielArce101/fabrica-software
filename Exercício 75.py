cdb61141b4bef2643b01b1aef49bafecb502cfdc
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:22:31 -0400

    Create Exercício 75.py

---
+++ Exercício 75.py
@@ -0,0 +1,6 @@
+#Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+numeros=[0,1,2,3,4,5,6,7,8,9,10]
+print("De tras pra frente")
+for al in range(-1,-12,-1):
+    print(f'Os números de trás pra frente são:\n{numeros[al]}')
+print('➖'*100)
