2795cac5314b303f8ff6681a6ee3d4d52bf2da6d
GabrielArce101 <117094290+GabrielArce101@users.noreply.github.com>
2023-02-06 10:24:42 -0400

    Create Exercício 80.py

---
+++ Exercício 80.py
@@ -0,0 +1,31 @@
+idade = int(input("Digite a idade:\n"))
+altura = float(input("Digite a altura:\n"))
+
+dados_da_pessoa1={
+        'A idade da pessoa 1 é:':idade,
+        'A altura da pessoa 1 é:':altura,}
+
+idade = int(input("Digite a idade:\n"))
+altura = float(input("Digite a altura:\n"))
+dados_da_pessoa2={
+        'A idade da pessoa 2 é:':idade,
+        'A altura da pessoa 2 é:':altura,}
+
+idade = int(input("Digite a idade:\n"))
+altura = float(input("Digite a altura:\n"))
+dados_da_pessoa3={
+        'A idade da pessoa 3 é:':idade,
+        'A altura da pessoa 3 é:':altura,}
+
+idade = int(input("Digite a idade:\n"))
+altura = float(input("Digite a altura:\n"))
+dados_da_pessoa4={
+        'A idade da pessoa 4 é:':idade,
+        'A altura da pessoa 4 é:':altura,}
+
+idade = int(input("Digite a idade:\n"))
+altura = float(input("Digite a altura:\n"))
+dados_da_pessoa5={
+        'A idade da pessoa 5 é:':idade,
+        'A altura da pessoa 5 é:':altura,}
+print(dados_da_pessoa1,dados_da_pessoa2,dados_da_pessoa3,dados_da_pessoa4,dados_da_pessoa5)
